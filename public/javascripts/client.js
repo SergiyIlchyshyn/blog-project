5a33cf000f88ad7291258adb92c7f62c446be1a2
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-05-07 23:55:13 +0300

    fixed view posts table

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,4 +1,10 @@
-// Call the dataTables jQuery plugin
 $(document).ready(function() {
+    // Call the dataTables jQuery plugin
     $('#dataTable').DataTable();
+
+    // Toggle the side navigation
+    $("#sidebarToggle").on("click", function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
 });
